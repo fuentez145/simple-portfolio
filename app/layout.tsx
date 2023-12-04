876598cbdf329f65920cbbdef7b0b3009f281076
fuentez145 <gilbert.fuentes@ctu.edu.ph>
2023-12-04 12:55:24 +0800

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from '@/components/NavBar'
 import { PTSans } from '@/components/fonts/google'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -15,9 +16,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
   return (
     <html lang="en">
-      <body className={`${inter.className} dark`} >
+      <body className={`${inter.className} darkk`} >
         <NavBar />
         {children}</body>
     </html>
